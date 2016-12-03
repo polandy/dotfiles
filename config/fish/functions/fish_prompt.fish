e0585ab62511467d26f7410550face6441441199
polandy <code@pollari.ch>
2016-12-03 12:54:37 +0100

    override the prompt_pwd function in fish_prompt.fish to show the full path in the path

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -57,3 +57,12 @@ function fish_prompt --description 'Write out the prompt'
 
   set_color normal
 end
+
+# Override prompt_pwd
+function prompt_pwd --description 'Print the current working directory, NOT shortened to fit the prompt'
+  if test "$PWD" != "$HOME"
+        printf "%s" (echo $PWD|sed -e 's|/private||' -e "s|^$HOME|~|")
+    else
+        echo '~'
+    end
+end
