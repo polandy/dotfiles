401dd8aafa9b6e9e1205c8ca5062182bd41b953e
polandy <pollari@gmx.ch>
2017-01-08 14:24:18 +0100

    replaced some git aliases with abbreviations

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -25,42 +25,51 @@ git config --global alias.br "branch"
 git config --global alias.f "fetch"
 git config --global alias.d "diff"
 git config --global alias.dc "diff --cached"
-git config --global alias.dt "difftool -y"
-git config --global alias.dtc "difftool --cached -y"
+# git config --global alias.dt "difftool -y"
+# git config --global alias.dtc "difftool --cached -y"
 git config --global alias.up "pull --rebase"
-git config --global alias.co "checkout"
-git config --global alias.ci "commit"
+# git config --global alias.co "checkout"
+# git config --global alias.ci "commit"
 git config --global alias.rb "rebase"
 git config --global alias.lol "log --graph --pretty=oneline --abbrev-commit --decorate"
 git config --global alias.lola "log --graph --pretty=oneline --abbrev-commit --decorate --all"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
 
 # abbreviations
 abbr -a g='git'
 abbr -a gp='git push'
 abbr -a gpa='git push --all'
-abbr -a gu='git up'
-abbr -a gl='git log'
+abbr -a gu='git pull --rebase'
+abbr -a gl='git lg'
+abbr -a glgf="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 abbr -a gs='git status'
-abbr -a gdt='git dt'
+abbr -a gdt='git difftool -y'
 abbr -a gdtd='git difftool -d'
 abbr -a gd='git diff'
-abbr -a gdtc='git dtc' 
+abbr -a gdtc='git difftool --cached -y' 
 abbr -a gm='git commit -m'
+abbr -a gcamend='git commit --amend'
 abbr -a gma='git commit -am'
 abbr -a gb='git branch'
 abbr -a gba='git branch -a'
-abbr -a gc='git checkout'
+abbr -a gco='git checkout'
 abbr -a gcb='git checkout -b'
-abbr -a gr='git remote'
+abbr -a gci='git commit'
+abbr -a gr='git rebase'
 abbr -a grv='git remote -v'
 abbr -a gcd='git rev-parse 2>/dev/null && cd "./$(git rev-parse --show-cdup)"'
 abbr -a glol='git lol'
 abbr -a glola='git lola'
+abbr -a gstash='git stash'
 abbr -a gua="find . -maxdepth 3 -type d -name .git | xargs dirname |xargs -P 40 -n 1 -I '{}' sh -c 'cd {}; git pull'"
 abbr -a gsa="find . -maxdepth 3 -type d -name .git | xargs dirname |xargs -n 1 -I '{}' sh -c 'cd {}; git status'"
 abbr -a gau="git add -u :/"
 abbr -a ga="git add "
-#
+abbr -a stash="git stash "
+abbr -a pop="git stash pop"
+
+
 # Add all files by default
 function gaa
   set arg $argv .
@@ -68,7 +77,8 @@ function gaa
 end
 
 # Ruby enVironment Manager
-rvm default
+# rvm default
+# rvm use 2.2
 
 # Current branch or SHA if detached.
 alias gbs='git branch | perl -ne '"'"'/^\* (?:\(detached from (.*)\)|(.*))/ && print "$1$2"'"'"''
