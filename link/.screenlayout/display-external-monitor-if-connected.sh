1aa0b5f9e396bce49ea541da1093cddc8449825f
Andy Pollari <pollari@gmx.ch>
2017-08-20 14:57:59 +0200

    display script: unified names of the associative arrays

--- link/.screenlayout/display-external-monitor-if-connected.sh
+++ link/.screenlayout/display-external-monitor-if-connected.sh
@@ -15,11 +15,11 @@ SCREEN_INTERNAL=DP-4
 MONITOR=$SCREEN_INTERNAL
 
 # The configs for the displays are stores in the following associative arrays
-declare -A monitor_screen
-monitor_screen=([$MONITOR_INTERNAL]="DP-4" [$MONITOR_DP]="DP-3" [$MONITOR_HDMI]="DP-1")
+declare -A monitor_screens
+monitor_screens=([$MONITOR_INTERNAL]="DP-4" [$MONITOR_DP]="DP-3" [$MONITOR_HDMI]="DP-1")
 
-declare -A monitor_script
-monitor_script=([$MONITOR_INTERNAL]="1xinternalScreen.sh" [$MONITOR_DP]="1x32inch-dp.sh" [$MONITOR_HDMI]="1x32inch-hdmi.sh")
+declare -A monitor_scripts
+monitor_scripts=([$MONITOR_INTERNAL]="1xinternalScreen.sh" [$MONITOR_DP]="1x32inch-dp.sh" [$MONITOR_HDMI]="1x32inch-hdmi.sh")
 
 declare -A xresources_files
 xresources_files=([$MONITOR_INTERNAL]="Xresources-internal-display" [$MONITOR_DP]="Xresources-external-display" [$MONITOR_HDMI]="Xresources-external-display")
@@ -40,7 +40,7 @@ function set_dpi_for_firefox {
 
 function do_activate_monitor {
     monitor_to_activate=$1
-    xrandr_script="$HOME/.screenlayout/${monitor_script[$1]}"
+    xrandr_script="$HOME/.screenlayout/${monitor_scripts[$1]}"
     xresources_file="$HOME/.screenlayout/${xresources_files[$1]}"
     xrandr_dpi=${xrandr_dpis[$1]}
     firefox_dpi=${firefox_dpis[$1]}
@@ -66,16 +66,16 @@ function is_screen_connected {
 }
 
 # Iterate over all keys [HDMI, DP, INTERNAL]
-for monitor in "${!monitor_screen[@]}"; do
-  echo "checking configured monitor $monitor (${monitor_screen[$monitor]})"
-  if ! is_monitor_active $monitor && is_screen_connected "${monitor_screen[$monitor]}"
+for monitor in "${!monitor_screens[@]}"; do
+  echo "checking configured monitor $monitor (${monitor_screens[$monitor]})"
+  if ! is_monitor_active $monitor && is_screen_connected "${monitor_screens[$monitor]}"
   then
-    echo "$monitor (${monitor_screen[$monitor]}) is connected"
+    echo "$monitor (${monitor_screens[$monitor]}) is connected"
     do_activate_monitor $monitor
 #   ~/.dotfiles/util/set-mouse-acceleration.sh
     break
   else
-    printf "$monitor (${monitor_screen[$monitor]}) is not connected\n"
+    printf "$monitor (${monitor_screens[$monitor]}) is not connected\n"
   fi
 done
 
