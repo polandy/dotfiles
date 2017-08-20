fdf80e16e5cdab74b27a3476997af71079188afa
Andy Pollari <pollari@gmx.ch>
2017-08-20 23:09:21 +0200

    display script: improved, handles now connection of hdmi cable with thunderbolt adapter

---
+++ link/.screenlayout/1x32inch-hdmi-tb.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xrandr --output DP-6 --off \
+       --output DP-1 --off \
+       --output DP-4 --off \
+       --output DP-5 --mode 3840x2160 --pos 0x0 --rotate normal \
+       --output DP-2 --off \
+       --output DP-3 --off \
+       --output DP-0 --off
