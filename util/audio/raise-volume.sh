0cbac794d5e45610f343ce9fa30795348e93aa21
polandy <code@pollari.ch>
2017-08-24 00:02:21 +0200

    set sink volume for all sinks more generic

---
+++ util/audio/raise-volume.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get a a list of all sinks
+sinks=$(pacmd list-sinks | grep index | cut -d':' -f2 | cut -d' ' -f2)
+
+for sink in $sinks; do
+  cmd="pactl set-sink-volume $sink +5%"
+  echo "executing: $cmd"
+  eval $cmd
+done
