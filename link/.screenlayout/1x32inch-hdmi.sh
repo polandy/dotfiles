0c341ed378c174d683d07af147790ee6e4df95a9
polandy <code@pollari.ch>
2018-07-21 18:03:20 +0200

    moved screen layout scripts to bin directory

--- link/.screenlayout/1x32inch-hdmi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xrandr --output DP-6 --off \
-       --output DP-5 --off \
-       --output DP-4 --off \
-       --output DP-1 --mode 3840x2160 --pos 0x0 --rotate normal \
-       --output DP-2 --off \
-       --output DP-3 --off \
-       --output DP-0 --off
