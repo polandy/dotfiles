9b030174caede45f839e1be3dfc0fdb71404b932
Andy Pollari <pollari@gmx.ch>
2019-09-28 00:19:30 +0200

    screenlayouts: always set a primary screen

--- link/.screenlayout/1x32inch-hdmi-tb.sh
+++ link/.screenlayout/1x32inch-hdmi-tb.sh
@@ -2,7 +2,7 @@
 xrandr --output DP-6 --off \
        --output DP-1 --off \
        --output DP-4 --off \
-       --output DP-5 --mode 3840x2160 --pos 0x0 --rotate normal \
+       --output DP-5 --mode 3840x2160 --pos 0x0 --rotate normal --primary \
        --output DP-2 --off \
        --output DP-3 --off \
        --output DP-0 --off
