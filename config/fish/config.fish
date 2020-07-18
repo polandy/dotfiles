8bea664b559fa950f02917097f26300e7f9353d8
polandy <code@pollari.ch>
2020-07-18 09:55:58 +0200

    remove unused git alias from fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,21 +19,6 @@ git config --global alias.ad add
 git config --global alias.br branch
 git config --global alias.subup "submodule update --init --recursive"
 git config --global alias.subst "submodule status --recursive"
-
-# NDBJS git alias
-git config --global alias.s "status"
-git config --global alias.br "branch"
-git config --global alias.f "fetch"
-git config --global alias.d "diff"
-git config --global alias.dc "diff --cached"
-# git config --global alias.dt "difftool -y"
-# git config --global alias.dtc "difftool --cached -y"
-git config --global alias.up "pull --rebase"
-# git config --global alias.co "checkout"
-# git config --global alias.ci "commit"
-git config --global alias.rb "rebase"
-git config --global alias.lol "log --graph --pretty=oneline --abbrev-commit --decorate"
-git config --global alias.lola "log --graph --pretty=oneline --abbrev-commit --decorate --all"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 
