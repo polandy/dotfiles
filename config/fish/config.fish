952caee17f76fea959915d851d376850e38f6a75
polandy <code@pollari.ch>
2023-12-17 10:21:16 +0100

    remove unused ruby config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,6 @@
 # dotfiles related stuff
-set -gx PATH $PATH ~/.dotfiles/bin
-set -gx PATH $PATH ~/bin
-set -gx JAVA_HOME /usr/lib/jvm/default/
-set -gx M2_HOME /opt/maven
-set -gx HOST_NAME (cat /etc/hostname)
+set -a PATH ~/.dotfiles/bin
+set -a PATH ~/bin
 
 # locale
 set -g LANG en_US.utf8
@@ -22,23 +19,7 @@ git config --global alias.ad add
 git config --global alias.br branch
 git config --global alias.subup "submodule update --init --recursive"
 git config --global alias.subst "submodule status --recursive"
-
-# NDBJS git alias
-git config --global alias.s "status"
-git config --global alias.br "branch"
-git config --global alias.f "fetch"
-git config --global alias.d "diff"
-git config --global alias.dc "diff --cached"
-# git config --global alias.dt "difftool -y"
-# git config --global alias.dtc "difftool --cached -y"
-git config --global alias.up "pull --rebase"
-# git config --global alias.co "checkout"
-# git config --global alias.ci "commit"
-git config --global alias.rb "rebase"
-git config --global alias.lol "log --graph --pretty=oneline --abbrev-commit --decorate"
-git config --global alias.lola "log --graph --pretty=oneline --abbrev-commit --decorate --all"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-
 
 # abbreviations
 abbr -a g 'git'
@@ -51,7 +32,7 @@ abbr -a gs 'git status'
 abbr -a gdt 'git difftool -y'
 abbr -a gdtd 'git difftool -d'
 abbr -a gd 'git diff'
-abbr -a gdtc 'git difftool --cached -y' 
+abbr -a gdtc 'git difftool --cached -y'
 abbr -a gm 'git commit -m'
 abbr -a gcamend 'git commit --amend'
 abbr -a gma 'git commit -am'
@@ -65,6 +46,7 @@ abbr -a grv 'git remote -v'
 abbr -a gcd 'git rev-parse 2>/dev/null && cd "./$(git rev-parse --show-cdup)"'
 abbr -a glol 'git lol'
 abbr -a glola 'git lola'
+
 abbr -a gstash 'git stash'
 abbr -a gua "find . -maxdepth 3 -type d -name .git | xargs dirname |xargs -P 40 -n 1 -I '{}' sh -c 'cd {}; git pull'"
 abbr -a gsa "find . -maxdepth 3 -type d -name .git | xargs dirname |xargs -n 1 -I '{}' sh -c 'cd {}; git status'"
@@ -73,9 +55,12 @@ abbr -a ga "git add "
 abbr -a stash "git stash "
 abbr -a pop "git stash pop"
 
-
-# Current branch or SHA if detached.
-alias gbs='git branch | perl -ne '"'"'/^\* (?:\(detached from (.*)\)|(.*))/ && print "$1$2"'"'"''
+# docker stuff
+abbr -a dc 'docker-compose'
+abbr -a dcu 'docker-compose up'
+abbr -a dcud 'docker-compose up -d'
+abbr -a dclf 'docker-compose logs -f'
+abbr -a dce 'docker-compose exec app'
 
 # vim related stuff
 alias vi="vim"
@@ -83,6 +68,12 @@ alias vim="nvim -p"
 set -x EDITOR vi
 set -x VISUAL_EDITOR nvim -p -u ~/.vim/.vimrc
 
+# misc aliases
+alias ssh="env TERM=xterm ssh"
+
 if test -e ~/.config/fish/config_local.fish
   . ~/.config/fish/config_local.fish
 end
+
+# golang
+set -g GOPATH ~/go
