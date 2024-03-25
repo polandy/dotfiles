356151ea704a8ba5d44a0b084caf766661dd9d28
polandy <code@pollari.ch>
2024-03-25 23:36:04 +0100

    configure steam floating windows and add display configuration for display port

---
+++ link/.screenlayout/1x32inch-dp-coolmaster.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+xrandr \
+--output HDMI-0 --off \
+--output DP-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
+--output DP-1 --off \
+--output DP-2 --off \
+--output DP-3 --off \
+--output DP-4 --off \
+--output DP-5 --off \
+--output None-2-1 --off \
+--output HDMI-1-1 --off \
+--output DP-1-1 --off \
