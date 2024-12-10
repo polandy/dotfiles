10afd8eedd205d4ff2860c0190cf548916cf1425
Andy Pollari <code@pollari.ch>
2024-12-10 22:14:09 +0100

    configure i3 for coolermaster

--- link/.screenlayout/1x32inch-dp-coolmaster.sh
+++ link/.screenlayout/1x32inch-dp-coolmaster.sh
@@ -2,6 +2,7 @@
 xrandr \
 --output HDMI-0 --off \
 --output DP-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
+--output DP-2-1         --mode 1920x1200 --pos 3840x1465 --rotate normal \
 --output DP-1 --off \
 --output DP-2 --off \
 --output DP-3 --off \
@@ -10,3 +11,7 @@ xrandr \
 --output None-2-1 --off \
 --output HDMI-1-1 --off \
 --output DP-1-1 --off \
+--output DP-2-1 --off \
+
+echo $(date "+%Y-%m-%d %H:%M:%S") >> /tmp/display-debug.log
+
