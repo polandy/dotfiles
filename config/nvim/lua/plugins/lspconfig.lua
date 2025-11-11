eb85d84f74f2d1d16362a7c1675a5dc1c5d354fa
Andy Pollari <andy@pollari.ch>
2025-11-11 22:09:43 +0100

    nvim: update plugins

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -17,13 +17,6 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-        end)
-      end,
     },
     ---@class PluginLspOpts
     opts = {
@@ -33,7 +26,6 @@ return {
         tsserver = {},
       },
       -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
       ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
       setup = {
         -- example to setup with typescript.nvim
@@ -41,9 +33,24 @@ return {
           require("typescript").setup({ server = opts })
           return true
         end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
       },
     },
+    -- Use the config function to define LspAttach logic
+    config = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("TypescriptAttach", { clear = true }),
+        callback = function(args)
+          -- Ensure the attached client is tsserver before setting keymaps
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client and client.name == "tsserver" then
+            local buffer = args.buf
+            -- Set your custom keymaps here
+            -- stylua: ignore
+            vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+            vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+          end
+        end,
+      })
+    end,
   },
 }
